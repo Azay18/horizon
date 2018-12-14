11776a58cb1957dda6c985ee3486e2c28b74bd9f
Azay18 <muhammadrizqiaz@gmail.com>
2018-12-15 01:29:24 +0700

    nambahin fungsi

--- application/models/M_model.php
+++ application/models/M_model.php
@@ -19,23 +19,41 @@ class M_model extends CI_Model{
         // return $this->db->get($this->_table)->result();
     }
 
-    function daftar($data) {
+    public function daftar($data) {
         $this->db->insert('m_users',$data);
     }
 
-    function masuk($email,$password) {
+    public function masuk($email,$password) {
 		$query=$this->db->query("SELECT * FROM m_users WHERE email='$email' AND password=MD5('$password') LIMIT 1");
 		return $query;
     }
     
-    function terima_fasilitator($id){
+    public function terima_fasilitator($id){
         $this->uri->segment($id);
         $data = "aktif";
         $this->db->replace('m_users',$data);
     }
 
-    function tambah_dataDestana($data) {
+    public function InsertData($tableName, $data) {
+		$res = $this->db->insert($tableName, $data);
+		return $res;
+    }
+    
+    public function tambah_dataDestana($data) {
         $this->db->insert('m_data_destana',$data);
+    }
+
+    public function tambah_dataLaporan($data) {
+        $this->db->insert('m_laporan',$data);
+    }
+
+    public function get_data_destana($data) {
+        $des = $this->db->query('SELECT m_data_destana.nama_destana FROM m_data_destana');
+		return $des->result_array();
+    }
+
+    public function get_nama_fasilitator($email,$password) {
+        $nam = $this->db->query("SELECT m_users.nama WHERE email='$email' AND password=MD5('$password') LIMIT 1");
     }
   }
 
