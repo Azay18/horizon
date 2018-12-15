d167797af46b4b1b001e80f26a9edd3ca636e0d5
khatami99 <sauqikhatami084@gmail.com>
2018-12-15 15:43:57 +0700

    peta

--- application/views/peta.php
+++ application/views/peta.php
@@ -1,7 +1,10 @@
 <title>Laporan Pembangunan</title>
 <?php require 'f_header.php'; ?>
 
-<head><?php echo $map['js']; ?></head>
-<body><?php echo $map['html']; ?></body>
-
+<div class="container pt-4">
+    <div class="mt-5">
+        <head><?php echo $map['js']; ?></head>
+        <body><?php echo $map['html']; ?></body>
+    </div>
+</div>
 <?php require 'footer.php'; ?>
