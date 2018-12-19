c32991526fee2a17f0263645682c4743dd8815a6
khatami99 <sauqikhatami084@gmail.com>
2018-12-19 07:43:20 +0700

    h db

--- db_tugasbesar.sql
+++ db_tugasbesar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2018 at 07:55 PM
+-- Generation Time: Dec 19, 2018 at 01:41 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -47,15 +47,11 @@ CREATE TABLE `m_data_destana` (
 --
 
 INSERT INTO `m_data_destana` (`id_destana`, `nama_destana`, `nama_kepala_destana`, `jenis_destana`, `jumlah_penduduk`, `alamat`, `provinsi`, `kabupaten`, `kecamatan`, `kelurahan`, `kode_pos`) VALUES
-(7, 'Kimpulang', '', 'Belum Dikategorikan', 341, 'Jalan Kaliurang KM 14,5', 'Yogyakarta', 'Sleman', 'Sleman', '', 23123),
-(8, '', '', '', 0, '', '', '', '', '', 0),
-(9, '', '', '', 0, '', '', '', '', '', 0),
-(10, '', '', '', 0, '', '', '', '', '', 0),
-(11, '', '', '', 0, '', '', '', '', '', 0),
-(12, '', '', '', 0, '', '', '', '', '', 0),
-(13, '', '', '', 0, '', '', '', '', '', 0),
-(14, '', '', '', 0, '', '', '', '', '', 0),
-(15, '', '', 'Utama', 0, '', '', '', '', '', 0);
+(7, 'Kimpulan', '', 'Belum Dikategorikan', 341, 'Jalan Kaliurang KM 14,5', 'Yogyakarta', 'Sleman', 'Sleman', '', 23123),
+(16, 'Ambarketawang', '', 'Belum Dikategorikan', 19237, '', 'Yogyakarta', 'Sleman', 'Gamping', '', 55294),
+(17, 'Imogiri', '', 'Belum Dikategorikan', 10000, '', 'Yogyakarta', 'Bantul', 'Imogiri', '', 55212),
+(18, 'Maguwoharjo', '', 'Madya', 20100, '', 'Yogyakarta', 'Sleman', 'Depok', '', 55321),
+(19, 'Mulo', '', 'Bukan Desa Tangguh Bencana', 21000, '', 'Yogyakarta', 'Gunung Kidul', 'Wonosari', '', 55231);
 
 -- --------------------------------------------------------
 
@@ -95,7 +91,7 @@ CREATE TABLE `m_laporan` (
   `tidak5` int(11) NOT NULL,
   `tidak6` int(11) NOT NULL,
   `tidak7` int(11) NOT NULL,
-  `jenis_destana` varchar(100) NOT NULL
+  `jenis_destana` enum('Belum Dikategorikan','Utama','Madya','Pratama','Bukan Desa Tangguh Bencana') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -103,8 +99,11 @@ CREATE TABLE `m_laporan` (
 --
 
 INSERT INTO `m_laporan` (`id_laporan`, `tanggal_laporan`, `nama_fasilitator`, `nama_desa`, `jumlah_ya`, `jumlah_tidak`, `ya1`, `ya2`, `ya3`, `ya4`, `ya5`, `ya6`, `ya7`, `tidak1`, `tidak2`, `tidak3`, `tidak4`, `tidak5`, `tidak6`, `tidak7`, `jenis_destana`) VALUES
-(15, '2018-12-18', 'Patimura', 'Kimpulan', 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 'Belum Dikategorikan'),
-(16, '2018-12-19', 'Patimura', 'Kimpulang', 5, 5, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 'Belum Dikategorikan');
+(17, '2018-12-19', 'Patimura', 'Maguwoharjo', 6, 6, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 'Madya'),
+(20, '2018-12-19', 'Habib Faizal', 'Mulo', 4, 4, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 'Bukan Desa Tangguh Bencana'),
+(21, '2018-12-19', 'Patimura', 'Kimpulan', 2, 2, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 'Belum Dikategorikan'),
+(22, '2018-12-19', 'Habib Faizal', 'Imogiri', 4, 4, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 'Belum Dikategorikan'),
+(23, '2018-12-19', 'Azay', 'Ambarketawang', 5, 5, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 'Belum Dikategorikan');
 
 -- --------------------------------------------------------
 
@@ -163,16 +162,17 @@ INSERT INTO `m_users` (`id_user`, `nama`, `jenis_kelamin`, `usia`, `jenjang_pend
 (2, 'Muhammad Sauqi Khatami', '', 0, '', '', '17523142@students.uii.ac.id', 'f33f9d002da76eac709a6a81fe96aff5', '', 0, 0, '', 'aktif', 1),
 (4, 'Rara Erlon', 'Pria', 0, '', '', 'hahaha@gmail.com', 'f33f9d002da76eac709a6a81fe96aff5', 'Ahmad Yani', 0, 2147483647, 'fasilitator', 'aktif', 2),
 (5, 'Patimura', '', 0, '', '', 'patimura@gmail.com', 'b4162dc8e2b85460c47ebb1c315c46df', 'Maluku', 0, 147, 'fasilitator', 'aktif', 0),
-(6, 'Rara Erlin', 'Pria', 0, '', '', '17523143@students.uii.ac.id', 'dcc2f5c0b7286413585f7ff7fa9f69e3', 'Banjarmasin', 0, 2147483647, 'fasilitator', 'aktif', 3),
+(6, 'Rara Erlin', 'perempuan', 0, '', '', '17523143@students.uii.ac.id', 'dcc2f5c0b7286413585f7ff7fa9f69e3', 'Banjarmasin', 0, 2147483647, 'fasilitator', 'aktif', 3),
 (12, 'state', 'laki', 9, 'smp', 'qqqqqqqqqqq', 'esma@gmail.com', '9ed39e2ea931586b6a985a6942ef573e', 'Banjarbaru', 123123, 2193013, '', 'aktif', 0),
-(16, 'asdasde', 'laki', 230, 'sd', 'Seru', '17523132@students.uii.ac.id', '7815696ecbf1c96e6894b779456d330e', 'Jakarta', 1111, 90123809, 'fasilitator', 'nonaktif', 0),
 (17, 'Suta Rahmadhani', 'laki', 15, 'S1', 'ajikdako', 'sauqikhatami084@gmail.com', 'dae0b2d1d7d345571781a5c0b4546959', 'Jalan Ahmad Yani', 123123, 1231234213, 'fasilitator', 'nonaktif', 0),
 (18, 'Deddy Cobuser', 'laki', 99, 'sd', 'loh\r\n', 'deddy@gmail.com', '0c09376372e339d44d3b5dadb56df2d5', 'Sumatra', 70341, 2147483647, 'fasilitator', 'nonaktif', 0),
 (19, 'Harry Potter', 'laki', 25, 'S3', 'SSeru', 'hari@gmail.com', 'a9bcf1e4d7b95a22e2975c812d938889', 'Jawa', 123456, 2147483647, 'fasilitator', 'nonaktif', 0),
 (20, 'Muhammad Rozi', 'laki', 15, 'smp', 'yipie\r\n', 'rozi@gmail.com', '6b77cfa7c0de00cd481b82dc5c5d99fe', 'Jawa', 70523, 2147483647, 'fasilitator', 'aktif', 0),
 (25, 'Suta Rahmadhani Ahmad', 'laki', 21, 'sma', 'qf', 'ahmad@yahoo.com', '61243c7b9a4022cb3f8dc3106767ed12', 'Banjarmasin', 70523, 2193013, 'fasilitator', '', 0),
 (26, 'Rizqi Az Zayyad', 'laki', 33, 'S2', 'Ya', 'rizkie@gmail.com', '1829d0dd8831dcd96a9219ecef50a0cd', 'Banjarbaru', 123123, 2147483647, 'fasilitator', '', 0),
-(27, 'Riskayanti', 'perempuan', 12, 'S1', 'hee', 'riska@gmail.com', 'fb059ad1c514876b15b3ec40df1acdac', 'Banjarbaru', 123123, 2147483647, 'fasilitator', 'aktif', 0);
+(27, 'Riskayanti', 'perempuan', 12, 'S1', 'hee', 'riska@gmail.com', 'fb059ad1c514876b15b3ec40df1acdac', 'Banjarbaru', 123123, 2147483647, 'fasilitator', 'aktif', 0),
+(28, 'Azay', 'laki', 18, 'S2', 'suka', 'azay@gmail.com', '202cb962ac59075b964b07152d234b70', 'Tulip', 51151, 2147483647, 'fasilitator', 'aktif', 0),
+(29, 'Habib Faizal', 'laki', 19, 'S1', 'mantap', 'habib@gmail.com', '1391921ec73a2f5dff54e075bbda7487', 'jogja', 12121, 2147483647, 'fasilitator', 'aktif', 0);
 
 --
 -- Indexes for dumped tables
@@ -215,13 +215,13 @@ ALTER TABLE `m_users`
 -- AUTO_INCREMENT for table `m_data_destana`
 --
 ALTER TABLE `m_data_destana`
-  MODIFY `id_destana` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_destana` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `m_laporan`
 --
 ALTER TABLE `m_laporan`
-  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `m_not_users`
@@ -233,7 +233,7 @@ ALTER TABLE `m_not_users`
 -- AUTO_INCREMENT for table `m_users`
 --
 ALTER TABLE `m_users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
